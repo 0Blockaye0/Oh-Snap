e7bd600bcb4291919fc2147502d868e1bb66349b
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-07-01 10:27:08 -0500

    form complete

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function ContactForm() {
-    return (
-        <section>
-          <h1>Contact me</h1>
-          <form id="contact-form">
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input type="text" name="name" />
-            </div>
-            <div>
-              <label htmlFor="email">Email address:</label>
-              <input type="email" name="email" />
-            </div>
-            <div>
-              <label htmlFor="message">Message:</label>
-              <textarea name="message" rows="5" />
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        </section>
-        );
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
 
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  console.log(formState);
+
+  function handleSubmit(e) {
+      e.preventDefault();
+      console.log(formState);
+  }
+
+  return (
+    <section onChange={handleChange}>
+      <h1>Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            defaultValue={name}
+            name="name"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" defaultValue={message} rows="5" onChange={handleChange} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
-default export = ContactForm;
+export default ContactForm;
